80e7b57e3cee86ef9024e3b6a1344eee06a7181f
Matheus Ribeiro <matheusribeiro545@gmail.com>
2023-10-21 16:47:19 -0300

    Configuração complementar JWT

--- src/Services/Exemplo.Auth.API/Program.cs
+++ src/Services/Exemplo.Auth.API/Program.cs
@@ -1,5 +1,6 @@
 using System.Text;
 using Exemplo.Auth.API.Data;
+using Exemplo.Auth.API.Extensions;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
@@ -18,6 +19,12 @@ builder.Services.AddDefaultIdentity<IdentityUser>()
     .AddDefaultTokenProviders();
 
 // Configuração de autenticação, utilizando JWT
+var appSettingsSection = builder.Configuration.GetSection("AppSettings");
+builder.Services.Configure<AppSettings>(appSettingsSection);
+
+var appSettings = appSettingsSection.Get<AppSettings>();
+var key = Encoding.ASCII.GetBytes(appSettings.Secret);
+
 builder.Services.AddAuthentication(options =>
     {
         options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
@@ -29,28 +36,27 @@ builder.Services.AddAuthentication(options =>
         bearerOptions.TokenValidationParameters = new TokenValidationParameters
         {
             ValidateIssuerSigningKey = true,
-            IssuerSigningKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes("x")),
+            IssuerSigningKey = new SymmetricSecurityKey(key),
             ValidateIssuer = true,
             ValidateAudience = true,
-            ValidAudience = "x",
-            ValidIssuer = "x"
+            ValidAudience = appSettings.ValidoEm,
+            ValidIssuer = appSettings.Emissor
         };
     });
 
+// Middleware pipeline
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
 
-// Middleware pipeline
 app.UseHttpsRedirection();
 
 app.UseRouting();
