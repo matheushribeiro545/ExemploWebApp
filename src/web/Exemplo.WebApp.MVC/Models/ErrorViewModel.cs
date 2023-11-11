3440be39de50b1ee828865bf238144fdf074b9dc
Matheus Ribeiro <matheusribeiro545@gmail.com>
2023-11-11 18:59:18 -0300

    Criação do projeto MVC

---
+++ src/web/Exemplo.WebApp.MVC/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Exemplo.WebApp.MVC.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
